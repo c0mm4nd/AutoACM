5c14b5f766ba135241f634838c49b1afea9d1db7
Command M <maoxs2@163.com>
2016-11-20 19:48:36 +0800

    rename

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# ACM
-Spider for ACM
+# Project AutoACM ~~ACM~~
+~~Spider for ACM~~
+一个大三狗重写准大二时写的基于Spider的做题Robot。
+
+### 目标
+
+目前只有对杭州电子科技大学(HDU)OJ的自动做题
+
+会加入其他OJ
+
+### 使用方法
+
+因为用Node写的，大家务必先装上npm，然后切换至国内镜像。
+
+`npm install ` 安装依赖
+
+界面采用浏览器实现，故只需`npm run`
--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 var express = require('express');
+var bodyParser = require('body-parser'); // fucking express
+
 var cheerio = require('cheerio');
 var superagent = require('superagent');
+
 var globalCookie = undefined;
 
 var username = undefined , userpw = undefined;
 
 var app = express();
+app.use(bodyParser());// fucking express
+app.use(bodyParser.urlencoded({ extended: false })) // I just wanna use post & get ...
 
 var server = app.listen(3000, function (){
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Example app listening at http://%s:%s', host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
 });
 
-app.get('/index.php',function(req,res){
-	res.send('正在后台为您解题');
-	username = req.query.n;
-	userpw = req.query.p;
-	if (username != "" && userpw != "") {
-		login(username,userpw);
-		console.log("UserName: " + username + "\nUserPassword: " + userpw)
-	} 
-	else{
-		console.log('kong');
-	}
+app.all('/hduoj',function(req,res){
+    res.send('正在后台为您解题');
+    console.log(req)
+    username = req.body.n;
+    userpw = req.body.p;
+        console.log("UserName: " + username + "\nUserPassword: " + userpw)
+    if (username != "" && userpw != "") {
+        login(username,userpw);
+    }
+    else{
+        console.log('kong');
+    }
+});
+
+app.get('/', function (req, res) {
+    res.send("<form action='hduoj' method='post' accept-charset='utf-8'><input type='text' name='n' placeholder='Your StudentID'><input type='password' name='p' placeholder='Your Password'><input type='submit' name='' value='OK'></form>");
 });
 
 // 模拟代码提交 
 function post(code, problemId) { 
-	superagent
+    superagent
     // 代码 post 的 url 
     .post('http://acm.hdu.edu.cn/submit.php?action=submit') 
     .set('Content-Type', 'application/x-www-form-urlencoded')
@@ -39,38 +49,36 @@ function post(code, problemId) {
     .send({"problemid": problemId}) 
     .send({"usercode": code}) 
     .end(function (err, sres) {
-    	console.log(err)
+        if (err){console.log(err)}
     });
 } 
  
- 
-
-
 // 从 csdn 题解详情页获取代码 
 function getCode(solutionUrl, problemId) { 
+    superagent.get(solutionUrl, function(err, sres) { 
+        // 为防止该 solutionUrl 可能不是题解详情页 
+        // 可能会没有 class 为 cpp 的 dom 元素 
+        try{
+            var $ = cheerio.load(sres.text); 
+            // C++ 情况
+            if ($('.cpp').eq(0).text()){
+                var code = $('.cpp').eq(0).text();
+            }
+            // java 
+            // var code = $('.java').eq(0).text();
+            if ($('.java').eq(0).text()){
+                var code = $('.cpp').eq(0).text();   
+            }  
 
+            // pre code 【markdown生成
+            // 需过滤
+            if (!code){return;} 
+            post(code, problemId); 
 
-  	superagent.get(solutionUrl, function(err, sres) { 
-	    // 为防止该 solutionUrl 可能不是题解详情页 
-	    // 可能会没有 class 为 cpp 的 dom 元素 
-	    try { 
-	    	var $ = cheerio.load(sres.text); 
+        } catch(e) { 
+            console.log(e)
 
-	    	// C++ 情况
-	    	var code = $('.cpp').eq(0).text(); 
-
-	    	// java 
-			// var code = $('.java').eq(0).text(); 
-
-			// pre code 【markdown生成
-			// 需过滤
-	    	if (!code){return;} 
-	    	post(code, problemId); 
-
-	    } catch(e) { 
-	    	console.log(e)
-
-	    } 
+        } 
   }); 
 } 
 
@@ -79,28 +87,27 @@ function getCode(solutionUrl, problemId) {
 
 // 模拟百度搜索题解 
 function bdSearch(problemId) { 
-	var searchUrl = 'https://www.baidu.com/s?ie=UTF-8&wd=hdu' + problemId; 
-
-   	superagent 
+    var searchUrl = 'https://www.baidu.com/s?ie=UTF-8&wd=hdu' + problemId; 
+    superagent
     .get(searchUrl) 
     // 必带的请求头 
     .set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36") 
     .end(function(err, sres) { 
-    	var $ = cheerio.load(sres.text); 
-       	var lis = $('.t a'); 
-       	for (var i = 0; i < 10; i++) { 
-	       	var node = lis.eq(i); 
-	 
-	        // 获取那个小的 url 地址 
-	        var text = node.parent().next().next().children("a").text(); 
-	 
-	    	// 如果 url 不带有 csdn 字样，则返回 
-	    	if (text.toLowerCase().indexOf("csdn") === -1){continue;}
+        var $ = cheerio.load(sres.text); 
+        var lis = $('.t a'); 
+        for (var i = 0; i < 10; i++) { 
+            var node = lis.eq(i); 
+     
+            // 获取那个小的 url 地址 
+            var text = node.parent().next().next().children("a").text(); 
+     
+            // 如果 url 不带有 csdn 字样，则返回 
+            if (text.toLowerCase().indexOf("csdn") === -1){continue;}
 
-	    	// 题解详情页 url 
-	    	var solutionUrl = node.attr('href'); 
-	    	getCode(solutionUrl, problemId); 
-       	} 
+            // 题解详情页 url 
+            var solutionUrl = node.attr('href'); 
+            getCode(solutionUrl, problemId); 
+        } 
     }); 
 } 
  
@@ -109,23 +116,23 @@ function bdSearch(problemId) {
  
  // 模拟登录 
 function login() { 
-	superagent
+    superagent
      // get 请求任意 acm.hdu.edu.cn 域名下的一个 url 
      // 获取 key 为 PHPSESSID 这个 Cookie 
     .get('http://acm.hdu.edu.cn/status.php') 
     .end(function(err, sres) { 
-    	// 提取 Cookie 
-    	var str = sres.header['set-cookie'][0]; 
-    	// 过滤响应头 Cookie 中的 path 字段 
-    	var pos = str.indexOf(';'); 
+        // 提取 Cookie 
+        var str = sres.header['set-cookie'][0]; 
+        // 过滤响应头 Cookie 中的 path 字段 
+        var pos = str.indexOf(';'); 
  
 
-    	// 全局变量存储 Cookie，post 代码提交时候用 
-    	globalCookie = str.substr(0, pos); 
+        // 全局变量存储 Cookie，post 代码提交时候用 
+        globalCookie = str.substr(0, pos); 
  
 
-    	// 模拟登录 
-    	superagent 
+        // 模拟登录 
+        superagent 
         // 登录 url 
         .post('http://acm.hdu.edu.cn/userloginex.php?action=login') 
         // post 用户名 & 密码 
@@ -136,12 +143,12 @@ function login() {
         // 请求携带 Cookie 
         .set("Cookie", globalCookie) 
         .end(function(err, sres) { 
-        	// 登录完成后，启动程序 
-        	console.log("Successful Login : " + username);
-        	if (err != null){
-        		console.log(err) 
-           	} 
-            console.log(sres)
+            // 登录完成后，启动程序 
+            console.log("Successful Login : " + username);
+            if (err != null){
+                console.log(err) 
+            } 
+            // console.log(sres)
             start(); 
         });
     }); 
@@ -151,15 +158,14 @@ function login() {
  
 
  // 程序启动 
-function start() { 
-	for (var i = 0; i <= 5600; i++) { 
-	    var problemId = i , delay = (i - 1000)*10000; 
-
-    	(function(delay, problemId) { 
-    		setTimeout(function() { 
-    			bdSearch(problemId);   
-    	  	}, delay);
-    	}(delay, problemId)); 
+function start() {
+    for (var i = 0; i <= 5600; i++) { 
+        var problemId = i , delay = (i - 1000)*10000; 
+        (function(delay, problemId) { 
+            setTimeout(function() { 
+                bdSearch(problemId);   
+            }, delay);
+        }(delay, problemId)); 
     } 
 } 
 
@@ -169,29 +175,29 @@ function start() {
 // var Submitter = {}
 
 // CodeFinder.bdSearch = function (problemId) { 
-// 	var searchUrl = 'https://www.baidu.com/s?ie=UTF-8&wd=hdu' + problemId; 
+//  var searchUrl = 'https://www.baidu.com/s?ie=UTF-8&wd=hdu' + problemId; 
 
-//    	superagent 
+//      superagent 
 //     .get(searchUrl) 
 //     // 必带的请求头 
 //     .set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36") 
 //     .end(function(err, sres) { 
-//     	var $ = cheerio.load(sres.text); 
-//        	var lis = $('.t a'); 
-//        	for (var i = 0; i < 10; i++) { 
-// 	       	var node = lis.eq(i)
-	 
-// 	        // 获取那个小的 url 地址 
-// 	        var text = node.parent().next().next().children("a").text()
-	 
+//      var $ = cheerio.load(sres.text); 
+//          var lis = $('.t a'); 
+//          for (var i = 0; i < 10; i++) { 
+//          var node = lis.eq(i)
+     
+//          // 获取那个小的 url 地址 
+//          var text = node.parent().next().next().children("a").text()
+     
 
-// 	    	// 如果 url 不带有 csdn 字样，则返回 
-// 	    	if (text.toLowerCase().indexOf("csdn") === -1){continue}
+//          // 如果 url 不带有 csdn 字样，则返回 
+//          if (text.toLowerCase().indexOf("csdn") === -1){continue}
 
-// 	    	// 题解详情页 url 
-// 	    	var solutionUrl = node.attr('href')
-// 	    	getCode(solutionUrl, problemId)
-//        	} 
+//          // 题解详情页 url 
+//          var solutionUrl = node.attr('href')
+//          getCode(solutionUrl, problemId)
+//          } 
 //     })
 // } 
 //  
